c021e3d7c960c782ed9512b905db11294bd11f17
Fallou24 <falloundiaye0221@gmail.com>
2022-12-16 07:31:55 +0000

    Update Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,37 +5,40 @@ import "./contact.scss";
 const Contact = () => {
   return (
     <div className="contact section" id="contact">
-      <h1 className="section__title">Contact Me</h1>
+      <h1 className="talk__me-title">Contactez moi</h1>
+      <h3 className="contact__text">N'hesiter pas à me contacter via les moyens ci-dessous</h3>
       <div className="contact__container container">
         <div className="talk__me">
-          <h3 className="talk__me-title">Talk me</h3>
           <div className="contact__box">
             <i className="bx bx-envelope"></i>
             <span>Email</span>
             <span>falloundiaye0221@gmail.com</span>
-            <button>
-              <span>Write me </span>
+            <a href="mailto:falloundiaye0221@gmail.com" target="_blank">
+              <span>Écrivez moi </span>
               <i className="bx bx-right-arrow-alt"></i>
-            </button>
+            </a>
           </div>
           <div className="contact__box">
             <i className="bx bxl-whatsapp"></i>
             <span>Whatshap</span>
             <span>70 337 79 59</span>
-            <button>
-              <span>Write me </span>
+            <a href="https://wa.me/703377959" target="_blank">
+              <span>Écrivez moi </span>
               <i className="bx bx-right-arrow-alt"></i>
-            </button>
+            </a>
           </div>
 
           <div className="contact__box">
             <i className="bx bxl-twitter"></i>
             <span>Twitter</span>
 
-            <button>
-              <span>Write me </span>
+            <a
+              href="https://twitter.com/messages/1528020897105313800"
+              target="_blank"
+            >
+              <span>Écrivez moi </span>
               <i className="bx bx-right-arrow-alt"></i>
-            </button>
+            </a>
           </div>
         </div>
         <ContactForm />
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
 
 const ContactForm = () => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [alert, setAlert] = useState("");
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setIsFetching(true);
+    emailjs
+      .sendForm(
+        "service_n3e739r",
+        "template_vienyb8",
+        form.current,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          setIsFetching(false);
+          setAlert("Votre message est bien envoyé");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="contact__form">
-      <h3>Write me your project</h3>
-      <form>
+      <form onSubmit={sendEmail} ref={form}>
         <fieldset>
-          <legend>Name</legend>
-          <input type="text" placeholder="Insert your name" />
+          <legend>Nom</legend>
+          <input
+            required
+            type="text"
+            name="name"
+            placeholder="Entrez votre nom"
+          />
         </fieldset>
         <fieldset>
-          <legend>Mail</legend>
-          <input type="email" placeholder="Insert your email" />
+          <legend>Email</legend>
+          <input
+            required
+            type="email"
+            name="email"
+            placeholder="Entrez votre email"
+          />
         </fieldset>
         <fieldset className="textarea">
-          <legend>Project</legend>
-          <textarea placeholder="Insert your project"></textarea>
+          <legend>Message</legend>
+          <textarea
+            required
+            name="message"
+            placeholder="Entrez votre message"
+          ></textarea>
         </fieldset>
-        <button className="button send">
-          Send Message <i className="bx bxs-send"></i>
+        <button className="button" disabled={isFetching}>
+          {isFetching ? (
+            "Envoi en cours..."
+          ) : (
+            <span className="send">
+              Envoyer <i className="bx bxs-send"></i>{" "}
+            </span>
+          )}
         </button>
       </form>
+      {alert && <p className="alert">{alert}</p>}
     </div>
   );
 };
